2f3bca3565c91fd7335a86e578fbd077585cd2e3
Bowen Wang <bowenwang@172-17-86-249.DYNAPOOL.NYU.EDU>
2017-02-21 12:05:07 -0500

    "Fixed minor problem in User Interface"

---
+++ schoolreg- Beta 1.2/src/schoolreg/Database.java
@@ -0,0 +1,293 @@
+package schoolreg;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.Serializable;
+
+
+public class Database implements Serializable{
+	//public static ArrayList<Course> All_course = Course.get_all_course();
+	//public static ArrayList<Course> All_course;
+	public static ArrayList<Course> All_course ;
+	public static ArrayList<Student> student_list;
+	public static String message = "message";
+	
+	public Admin admin = new Admin("Admin","Admin001","first_name","last_name");
+	
+	
+	
+	
+	public Database(){
+		
+	}
+	public void set_message(){
+		this.message = "I changed it";
+	}
+	public ArrayList<Course> getCourses_list(){
+		return All_course;
+		
+	}
+	
+	public static void set_all_course(ArrayList x){
+		Database.All_course = x;
+	}
+	
+	public ArrayList<Student> getStudent_list(){
+		return student_list;
+	}
+	
+	public void display(){
+		//System.out.println(All_course.get(1).get_Course_Name());
+		Scanner input = new Scanner (System.in);
+		int Choice;
+		System.out.println("Choose the option below");
+		System.out.println("1-Admin, 2-Student, 3-exit");
+		Choice = input.nextInt();
+		input.nextLine();
+		switch (Choice){
+		case 3:break;
+		case 1: 
+			System.out.println("Enter Admin Menu");
+			String username = input.nextLine(); 
+			System.out.println("Enter password"); 
+			String password = input.nextLine(); 
+			if (username.equals("Admin")& password.equals("Admin001")){
+					System.out.println("Login successfully");
+					admin_display();
+			}
+			else{
+				System.out.println("Username or password not right");
+				display();
+			}
+			
+			break;
+				
+	    case 2: 
+	    	
+			System.out.println("Enter username");
+			String stud_username = input.nextLine(); 
+			//System.out.println(stud_username);		
+			System.out.println("Enter password"); 
+			String stud_password = input.nextLine(); 
+			//System.out.println(stud_password);
+			System.out.println("Enter first name");
+			String stud_firstname = input.nextLine(); 
+			System.out.println("Enter last name"); 
+			String stud_lastname = input.nextLine(); 
+			System.out.println("Enter id"); 
+			String id = input.nextLine(); 
+			Student st1 =new Student(stud_username,stud_password,stud_firstname,stud_lastname,id,null);
+			Stu_display(st1);
+			break;
+		
+	    default:
+				System.out.println("Invalid input, exit system");
+				System.exit(0);
+				
+				
+	}
+
+	
+}
+	
+	public void Stu_display(Student st1){
+		Scanner input = new Scanner (System.in);
+		System.out.println("HI"+st1.getFirstname()+st1.getlastname());
+		int Choice;
+		
+		System.out.println("1. view all class");
+		System.out.println("2. view all classes that are not full ");
+		System.out.println("3. Register on a course");
+		System.out.println("4. withdraw from a course: full name , the course");
+		System.out.println("5. view all courses that studnet is being registered in");
+		System.out.println("6. Exit");
+		
+		Choice = input.nextInt();
+		input.nextLine();
+		switch (Choice){
+		case 6: break;
+		case 1: 
+				st1.view_all_class();
+				Stu_display(st1);
+				return ;
+		case 2: 
+				st1.view_not_full_calss();
+				Stu_display(st1);
+				return;
+		case 3: 
+			System.out.println("Enter course name");
+			String cos1 = input.nextLine(); 
+			System.out.println("Enter course id");
+			String cos = input.nextLine(); 
+			//System.out.println("Enter section number"); 
+			//int sec2 = input.nextInt(); 
+			//nput.nextLine();
+			//System.out.println(Course.get_Course(cos,sec2));
+			System.out.println("----");
+			System.out.println("Enter section number"); 
+			int secnumber = input.nextInt(); 
+			input.nextLine();
+			System.out.println("----");
+			System.out.println("Enter student's full name");
+			String name = st1.getFirstname() + " "+st1.getlastname();
+			System.out.println("You are looking for course" + cos + "  secton #" +secnumber+ "  your name is "+ name);
+			st1.register_on_a_course(cos1,cos,secnumber,name);
+			Stu_display(st1);
+			break;
+		case 4: 
+			System.out.println("Enter course name");
+			String cos2 = input.nextLine(); 
+			System.out.println("Enter course id");
+			String cosid = input.nextLine(); 
+			System.out.println("Enter student's full name");
+			String name1 = st1.getFirstname() + " "+st1.getlastname();
+			System.out.println("Enter section number"); 
+			int sec1 = input.nextInt(); 
+			input.nextLine();
+			st1.withdraw_a_course(name1, cosid,sec1,cos2);
+			Stu_display(st1);
+			break;
+		case 5: 
+			System.out.println("Below is all the courses that student is on");
+			st1.student_class_list();
+			Stu_display(st1);
+			break;
+		
+		default:
+				System.out.println("Invalid input, exit system");
+				System.exit(0);	
+	
+
+	
+		}
+	
+
+}
+	public void admin_display(){
+		Scanner input = new Scanner (System.in);
+		int Choice;
+		System.out.println("1. create a new course");
+		System.out.println("2. Delete a course ");
+		System.out.println("3. Edit a course ");
+		System.out.println("4. Display information for a given course( by id)");
+		System.out.println("5. Register a student");
+		System.out.println("6. Exit");
+		System.out.println("7. View all course ");
+		System.out.println("8. View all courses that are FULL");
+		System.out.println("9. Write to file that list of course that are full");
+		System.out.println("10. View the names of the students being registered in a specific course");
+		System.out.println("11. View the list of courses that a given student is being registered on");
+		System.out.println("12.  Sort courses based on the current number of student registers");
+		Choice = input.nextInt();
+		input.nextLine();
+		System.out.println("you are in the admind menu");
+		switch (Choice){
+		case 6: break;
+		case 1: 
+			System.out.println("Enter course name");
+			String cos = input.nextLine(); 
+			System.out.println("Enter course id "); 
+			String id= input.nextLine(); 
+			System.out.println("Enter Maximum Students"); 
+			int max_num= input.nextInt();
+			input.nextLine();
+			System.out.println("Enter current Students"); 
+			int cur_num= input.nextInt(); 
+			input.nextLine();
+			System.out.println("Enter lists of names"); 
+			String lists= input.nextLine(); 
+			ArrayList<String> myList = new ArrayList<String>(Arrays.asList(lists));
+			System.out.println("Enter course instructor");
+			String teacher = input.nextLine(); 
+			System.out.println("Enter section number"); 
+			int sec_num= input.nextInt(); 
+			input.nextLine();
+			System.out.println("Enter course location");
+			String loc = input.nextLine(); 
+			admin.add_new_course(cos,id,max_num,cur_num, myList,teacher, sec_num,loc);
+			admin_display();
+			break;
+		case 2: 
+			System.out.println("Enter course id which you want to delete ");
+			String cos1 = input.nextLine(); 
+			System.out.println("Enter section number"); 
+			int sec_num1= input.nextInt(); 
+			input.nextLine();
+			admin.Delete_a_course(cos1,sec_num1);
+			admin_display();
+			break;
+		case 3: 
+			System.out.println("Enter course ID");
+			String cos3 = input.nextLine(); 
+			System.out.println("Enter section number"); 
+			int sec_num4= input.nextInt(); 
+			input.nextLine();
+			admin.Edit_course(cos3,sec_num4);
+			admin_display();
+			break;
+		case 4: 
+			System.out.println("Enter the course id , so we can display info");
+			String id3 = input.nextLine(); 
+			System.out.println("Enter section number"); 
+			int sec_num2= input.nextInt(); 
+			input.nextLine();
+			admin.Display_info(id3,sec_num2);
+			admin_display();
+			break;
+		case 5: 
+			System.out.println("Enter course ID");
+			String cos2 = input.nextLine(); 
+			System.out.println("Enter student's full name");
+			String name1 = input.nextLine(); 
+			System.out.println("Enter section number"); 
+			int sec_num3= input.nextInt(); 
+			input.nextLine();
+			admin.add_student(name1,cos2,sec_num3);
+			admin_display();
+			break;
+		case 7: 
+			System.out.println("viewing all class now");
+			admin.view_all_class();
+			admin_display();
+			break;
+		case 8: 
+			admin.view_all_full_class();
+			admin_display();
+			break;
+		case 9: 
+			try {
+				admin.write_file_of_list();
+				admin_display();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		case 10:
+			System.out.println("Enter the course id , so we can display names");
+			String id4 = input.nextLine(); 
+			admin.view_names_in_class(id4);
+			admin_display();
+			break;
+		case 11:
+			System.out.println("Enter full name ");
+			String fn = input.nextLine(); 
+			
+			admin.student_class(fn);
+			admin_display();
+			break;	
+		case 12: 
+			admin.sort_courses();
+			admin_display();
+			break;
+			
+		default:
+				System.out.println("Invalid input, exit system");
+				System.exit(0);	
+	
+		}
+		}	
+	
+}
---
+++ schoolreg- Beta 1.2/src/schoolreg/Program.java
@@ -0,0 +1,223 @@
+package schoolreg;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Program implements Serializable{
+	
+
+	
+
+	
+	public static ArrayList course_deserlaize(){
+		//File file1  =new File("CourseRegis.ser");
+		ArrayList<Course> arraylist= new ArrayList<Course>();
+	try
+    {
+        FileInputStream fis = new FileInputStream("course_arraylist.ser");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        arraylist = (ArrayList) ois.readObject();
+        ois.close();
+        fis.close();
+        System.out.println("Deserilization Course arraylist compelted");
+     }catch(IOException ioe){
+         ioe.printStackTrace();
+         return arraylist;
+      }catch(ClassNotFoundException c){
+         System.out.println("Class not found");
+         c.printStackTrace();
+         return arraylist;
+      }
+	return arraylist;
+    }
+	
+	public static ArrayList student_deserlaize(){
+		//File file1  =new File("CourseRegis.ser");
+		ArrayList<Course> arraylist= new ArrayList<Course>();
+	try
+    {
+        FileInputStream fis = new FileInputStream("student_arraylist.ser");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        arraylist = (ArrayList) ois.readObject();
+        ois.close();
+        fis.close();
+        System.out.println("Deserilization Student arraylist compelted");
+     }catch(IOException ioe){
+         ioe.printStackTrace();
+         return arraylist;
+      }catch(ClassNotFoundException c){
+         System.out.println("Class not found");
+         c.printStackTrace();
+         return arraylist;
+      }
+	return arraylist;
+    }	
+	public static void course_seralize(ArrayList All_course){
+		
+		try{
+	        FileOutputStream fos= new FileOutputStream("course_arraylist.ser");
+	        ObjectOutputStream oos= new ObjectOutputStream(fos);
+	        oos.writeObject(All_course);
+	        oos.close();
+	        fos.close();
+	        System.out.println("Serilization Course Arraylist compelted");
+	      }catch(IOException ioe){
+	           ioe.printStackTrace();
+	       
+}}
+	public static void student_seralize(ArrayList All_course){
+		
+		try{
+	        FileOutputStream fos= new FileOutputStream("student_arraylist.ser");
+	        ObjectOutputStream oos= new ObjectOutputStream(fos);
+	        oos.writeObject(All_course);
+	        oos.close();
+	        fos.close();
+	        System.out.println("Serilization Student Arraylist compelted");
+	      }catch(IOException ioe){
+	           ioe.printStackTrace();
+	       
+}}
+	public static Database deserlaize(){
+		//File file1  =new File("CourseRegis.ser");
+		Database arraylist = null;
+	try
+    {
+        FileInputStream fis = new FileInputStream("CourseRegis.ser");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        arraylist = (Database) ois.readObject();
+        //System.out.println(arraylist.All_course.get(0).get_Course_Name());
+        System.out.println(arraylist.All_course);
+        ois.close();
+        fis.close();
+        System.out.println("Deserilization compelted");
+     }catch(IOException ioe){
+         ioe.printStackTrace();
+         return null;
+      }catch(ClassNotFoundException c){
+         System.out.println("Class not found");
+         c.printStackTrace();
+         return null ;
+      }
+	
+	System.out.println(arraylist.message);
+	return arraylist;
+    }
+  
+	public static void seralize(Database datz){
+		datz.set_message();
+		System.out.println(datz.message);
+		System.out.println("Here");
+		try{
+	        FileOutputStream fos = new FileOutputStream("CourseRegis.ser");
+	        ObjectOutputStream oos= new ObjectOutputStream(fos);
+	        System.out.println(datz.All_course.get(0).get_Course_Name());
+	        oos.writeObject(datz);
+	        oos.close();
+	        fos.close();
+	        System.out.println("Serilization compelted");
+	      }catch(IOException ioe){
+	           ioe.printStackTrace();
+	       
+}
+  }
+	
+	public static void main(String[] args) {
+		
+		Database nyu = new Database();
+		
+		//nyu.getCourses_list();
+		//nyu.getStudent_list();
+	
+		//File file1 = ;
+		Path path1 = Paths.get("student_arraylist.ser");
+		//Path path2 = Paths.get("CourseRegis.ser");
+		Path path2 = Paths.get("course_arraylist.ser");
+		
+		if(Files.exists(path1)==false &&Files.exists(path2) ==false){//
+			System.out.println("The ser file is not there, read from csv file");
+				
+			ArrayList<Course> initiate_course= new ArrayList<Course>();
+			String csvFile = "bin/schoolreg/MyUniversityCourses.csv";
+			BufferedReader br = null;
+			try {
+				br = new BufferedReader(new FileReader(new File(csvFile)));
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			String cvsSplitBy = ",";
+			String line = "";	
+			
+			try {
+				String line1 = br.readLine(); // this will read the first line
+				
+				line1=null;
+				
+				while((line1 = br.readLine())!=null){
+		
+					String[] lineArray = line1.split(",");
+					
+					ArrayList<String> myList = new ArrayList<String>();
+					int max_stud =Integer.parseInt(lineArray[2]);
+					//System.out.println(lineArray[0]);
+					int cur_stud = Integer.parseInt(lineArray[3]);
+					int cur_sec= Integer.parseInt(lineArray[6]);
+					Course cos = new Course(lineArray[0],lineArray[1],max_stud,cur_stud,myList,lineArray[5],cur_sec,lineArray[7]);
+					initiate_course.add(cos);
+					Course.All_course.add(cos);
+					
+					//Course.set_all_course(nyu.getCourses_list());
+					
+				}
+				nyu.set_all_course(Course.All_course);
+				//System.out.println(Course.All_course.size());
+				//System.out.println(nyu.All_course.size());
+
+				//System.out.println(Course.All_course.get(5).get_Course_Locations());
+			} catch (NumberFormatException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//string_deserlaize();
+			//System.out.println(nyu.message);
+			//System.out.println(nyu);
+			//System.out.println(Course.All_course.get(1).get_Course_id());
+		}
+		
+	else{
+			System.out.println("The ser file has alreayd exsisted, then deserlaizae");
+			nyu.student_list = student_deserlaize();
+			nyu.All_course =course_deserlaize();
+			
+			//System.out.println(nyu.All_course.get(1).get_Course_Name());
+			//System.out.println(nyu..get(0));
+			//System.out.println(Course.All_course.size());
+			//System.out.println(nyu.All_course);
+			//Course.set_all_course(nyu.getCourses_list());
+			//Course.set_all_course(nyu.getCourses_list());
+			
+			Course.All_course =nyu.All_course;
+			}
+		
+		nyu.display();
+		
+		course_seralize(nyu.All_course);
+		student_seralize(nyu.student_list);
+		//seralize(nyu);
+		//course_seralize(Course.get_all_course());
+		//System.out.println(Course.All_course.get(1).get_Course_id());
+		
+				}
+}
+	
+
+
